98b500ca7c8d1f884628ab3ca7441927c2e12259
DESKTOP-RA2C26F\Owner <eric.j.vanasperen@ndsu.edu>
2018-06-12 22:14:55 -0500

    Added basic Mapping support

---
+++ Basic-ASCII-RPG/Mapping.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Text;
+
+namespace Basic_ASCII_RPG
+{
+    public class Mapping
+    {
+        public const char Wall = 'X';
+        public const char Floor = '.';
+        public const char StairUp = '>';
+        public const char StairDown = '<';
+
+        public const int BoardSizeX = 32;
+        public const int BoardSizeY = 16;
+        public const int BoardSizeZ = 2;
+
+        public char[,,] Board { get; set; }
+
+        public Mapping()
+        {
+            InitializeMap();
+        }
+
+        private void InitializeMap()
+        {
+            // Temporarily create a simple map.
+            // TODO - Implement better mapping functionality
+            Board = new char[BoardSizeZ, BoardSizeY, BoardSizeX]
+            {
+                {
+                    { Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall },
+                    { Wall, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Wall },
+                    { Wall, Floor, Floor, Floor, Floor, Floor, Floor, Floor, StairUp, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Wall },
+                    { Wall, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Wall },
+                    { Wall, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Wall },
+                    { Wall, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Wall },
+                    { Wall, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Wall },
+                    { Wall, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Wall },
+                    { Wall, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Wall },
+                    { Wall, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Wall },
+                    { Wall, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Wall },
+                    { Wall, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Wall },
+                    { Wall, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Wall },
+                    { Wall, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, StairDown, Floor, Wall },
+                    { Wall, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Wall },
+                    { Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall }
+                },
+                {
+                    { Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall },
+                    { Wall, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Wall },
+                    { Wall, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Wall },
+                    { Wall, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Wall },
+                    { Wall, Floor, Floor, Floor, Floor, StairUp, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Wall },
+                    { Wall, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Wall },
+                    { Wall, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Wall },
+                    { Wall, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Wall },
+                    { Wall, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Wall },
+                    { Wall, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Wall },
+                    { Wall, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Wall },
+                    { Wall, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, StairDown, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Wall },
+                    { Wall, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Wall },
+                    { Wall, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Wall },
+                    { Wall, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Floor, Wall },
+                    { Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall }
+                }
+            };
+        }
+
+        public void SetPlayerStartPosition(Player player)
+        {
+            Board[player.StartZ, player.StartY, player.StartX] = player.TileSymbol;
+        }
+
+        public void PrintGameWindow(Player player)
+        {
+            PrintPlayerStats(player);
+            PrintMap(player);
+            // Provides an easy way to keep track of positioning for troubleshooting
+            PrintPlayerPosition(player);
+        }
+
+        private void PrintMap(Player player)
+        {
+            // Variable to store the entire map as a single string to prevent flickering during rapid movement
+            var mapString = new StringBuilder();
+
+            Console.Write("\t\t ");
+            // Up/down
+            for (var z = player.Z; z == player.Z; z++)
+            {
+                // East/West
+                for(var y = 0; y < BoardSizeY; y++)
+                {
+                    // North/South
+                    for(var x = 0; x < BoardSizeX; x++)
+                    {
+                        mapString.Append(Board[z, y, x]);
+                    }
+                    // Formatting
+                    mapString.AppendLine();
+                    mapString.Append("\t\t ");
+                }
+            }
+            Console.Write(mapString);
+        }
+        
+        private static void PrintPlayerPosition(Player player)
+        {
+            Console.WriteLine("\n\t\tPrevious X: {0}\tX: {1}\tNext X: {2}", player.PreviousX, player.X, player.NextX);
+            Console.WriteLine("\t\tPrevious Y: {0}\tY: {1}\tNext Y: {2}", player.PreviousY, player.Y, player.NextY);
+            Console.WriteLine("\t\tPrevious Z: {0}\tZ: {1}\tNext Z: {2}", player.PreviousZ, player.Z, player.NextZ);
+        }
+
+        private static void PrintPlayerStats(Player player)
+        {
+            Console.WriteLine("\t\t\tHP: {0}\tMP: {1}", player.HP, player.MP);
+            Console.WriteLine("Level: {0}\tExperience: {1}\tExperience to next level: {2}", player.Level, player.Experience, player.ExperienceToLevel);
+            Console.WriteLine("Attack: {0}\tDefense: {1}\tStrength: {2}\tVitality: {3}", player.Attack, player.Defense, player.Strength, player.Vitality);
+            Console.WriteLine("Dexterity: {0}\tAgility: {1}\tWisdom: {2}\tIntelligence: {3}", player.Dexterity, player.Agility, player.Wisdom, player.Intelligence);
+            Console.WriteLine();
+        }
+    }
+}
