595ac68b447448d2a9941b7fdcd8d0db4f93ed71
DESKTOP-RA2C26F\Owner <eric.j.vanasperen@ndsu.edu>
2018-06-12 22:16:05 -0500

    Added a basic game loop

--- Basic-ASCII-RPG/Program.cs
+++ Basic-ASCII-RPG/Program.cs
@@ -1,15 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Basic_ASCII_RPG
 {
     class Program
     {
-        static void Main(string[] args)
+        private static ConsoleKey _input;
+        private static readonly Player _player = new Player();
+        private static readonly Mapping _map = new Mapping();
+
+        internal static void Main(string[] args)
         {
+            _map.SetPlayerStartPosition(_player);
+            _map.PrintGameWindow(_player);
+
+            GameLoop();
+        }
+
+        private static void GameLoop()
+        {
+            do
+            {
+                // Get any key the user presses
+                _input = Console.ReadKey().Key;
+
+                // Clear the console
+                Console.Clear();
+
+                // Update movement
+                _player.Move(_input, _map);
+
+                // Refresh the map
+                _map.PrintGameWindow(_player);
+            } while (_input != ConsoleKey.Escape);
+
+            Console.WriteLine("\n\t    Press any key to exit...");
+            Console.ReadKey();
         }
     }
 }
